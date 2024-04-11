c1937b14b959c1c2c312bdd99c7214ba1a99fda1
gu <gusera4325@gmail.com>
2024-04-11 14:52:58 -0300

    alre

---
+++ mysql_config.js
@@ -0,0 +1,13 @@
+
+
+    module.exports = {
+
+
+        host: 'localhost',
+        user: 'user_bd_tasks',
+        password: 'QL0P4TDcQGB2R97Djet7vXYHggatTZE4',
+        database: 'nodejs_tasks'
+        
+        
+        
+        }
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const { error } = require('console')
 const express = require('express')
-const { get } = require('http')
-
 const app = express()
-
 const mysql12= require('mysql2')
-const send = require('send')
+const mysql_config = require('./mysql_config')
 
 
 app.listen(3001, () =>{
@@ -15,54 +11,13 @@ console.log('serv no ceu igual teu pai')
 })
 
 
-const conection = mysql12.createConnection({
+//const conection = mysql12.createConnection(mysql_config)
+
+app.get('/', (req,res)=>{
 
 
-host: 'localhost',
-user: 'user_bd_tasks',
-password: 'QL0P4TDcQGB2R97Djet7vXYHggatTZE4',
-database: 'nodejs_tasks'
+res.send('oi')
 
-
-
-})
-
-conection.connect(erro =>{
-
-
-if(erro){
-
-console.log('Erro no sql ' + erro.message)
-return;
-}
-
-console.log('Conexão estabelecida')
-
-
-
-
-
-
-})
-
-app.get('/', (req, res)=>{
-
-
-
-conection.query('SELECT * FROM tasks', (err, results,fields)=>{
-
-
-    if(err){
-
-
-        console.log(err.message)
-        res.send('Erro ao obter a lista')
-    }
-    else{
-
-        res.send(results)
-    }
-})
 
 
 })
